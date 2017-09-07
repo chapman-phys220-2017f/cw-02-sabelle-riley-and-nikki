22ea58c0a979342b17a96f60a7ead99d1ce18e68
kenda106 <31449719+kenda106@users.noreply.github.com>
2017-09-07 12:54:15 -0700

    Update converge.py

--- converge.py
+++ converge.py
@@ -3,12 +3,11 @@
 k = input("Please enter a value for k")
 
 final_sum = 0
-next_sum = ((1/k)**2)
 def compute_sum(tol=1e-2):
     while next_sum > tol:
-
+        next_sum = ((1/k)**2)
         final_sum = (final_sum + next_sum)
-        
+        k+=1
     return(final_sum)    
 
 print(compute_sum)
