c647e33774908b17ad51c40338001e1e86f772e4
kenda106 <31449719+kenda106@users.noreply.github.com>
2017-09-09 14:27:49 -0700

    Update coord.py

--- coord.py
+++ coord.py
@@ -2,12 +2,19 @@
 
 n=4
 def coord_for(n, a=0, b=12):
-
     h=(b-a)/n
     list_int = []
     for i in range(n+1):
         list_int.append(a + i*h)
-
     return list_int
-
 print(coord_for(n))
+
+
+n=4
+def coord_while(n, a=0, b=12):
+    h=(b-a)/n
+    list_int=[]
+    while i in range(n+1):
+        list_int.append(a + i*h)
+    return list_int
+print(coord_while(n))
