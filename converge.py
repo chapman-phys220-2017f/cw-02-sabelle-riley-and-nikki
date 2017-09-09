3d7e61d56a220f55760d61739ff755c335205879
Riley <kenda106@mail.chapman.edu>
2017-09-09 22:12:48 +0000

    Classwork 2 - Converge

--- converge.py
+++ converge.py
@@ -9,9 +9,10 @@ def compute_sum(tol=1e-2):
         final_sum = (final_sum + next_sum)
         k+=1
 	next_sum = ((1/k)**2)
-    print("Final Sum:",final_sum)
+    print("Final Sum:",float(final_sum))
 compute_sum()
+compute_sum(.01)
+compute_sum(.001)
+compute_sum(.0001)
 
-compute_sum(1)
-compute_sum(.5)
-compute_sum(.25)
+#for k = .5 on lower and lower tolerances, compute_sum approaches 5)
