fe6648281a41d17ccfd6b8a35f3a9c775cfee4c0
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-18 01:12:33 -0700

    Instructor note

--- fibs.py
+++ fibs.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
+### INSTRUCTOR NOTE
+# Never use input. No production code will ask the user to type something.
+# Also, any code that is not a definition should be put in the main block.
 n = input ("Type an integer for the amount of fibonacci numbers you want")
+###
 
 def fibs(n):
     
@@ -12,14 +16,14 @@ def fibs(n):
         a,b = b,a+b
         n=n-1
     return(fib_list)
-#print(fibs(n))
+#print(fibs(n))              ### Remove extraneous comments like this
 
 def fibs_generator():
     a,b = 0, 1
     while True:
         yield b
         a,b = b, a+b
-    return g
+    return g                 ### No need for this return. It will never execute anyway.
         
-#g = fibs_generator()
+#g = fibs_generator()        ### Remove extraneous comments.
 #print([next(g) for _ in range(n)] )
