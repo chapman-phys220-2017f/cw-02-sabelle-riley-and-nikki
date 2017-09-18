e7deb68b24875a0ef21c49fc7cfe52ed27f2e8f8
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-18 01:15:31 -0700

    Instructor note

--- coord.py
+++ coord.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+### INSTRUCTOR NOTE
+# Be sure to specify "python3" above. CoCalc defaults to python2 still.
+###
 
 def coord_for(n, a, b):
     h=(b-a)/n
@@ -6,7 +9,11 @@ def coord_for(n, a, b):
     for i in range(n+1):
         list_int.append(a + i*h)
     return list_int
+
+### INSTRUCTOR NOTE
+# Do not have executable code like this outside of a main block.
 print(coord_for(n))
+###
 
 def coord_while(n,a,b):
     h=(b-a)/n
@@ -16,10 +23,10 @@ def coord_while(n,a,b):
         list_int.append(a + i*h)
         i+=1
     return list_int
-print(coord_while(n))
+print(coord_while(n))   ### See above
 
 def coord_comp(n,a,b):
     h=(b-a)/n
-    list_int=[3*x for x in range(n+1)]
+    list_int=[3*x for x in range(n+1)]   ### This should be [a + i*h for i in range(n+1)]
     return list_int
-print(coord_comp(n))
+print(coord_comp(n))    ### See above
