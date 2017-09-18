5b338ee7039433ac9c9cce23e1f14bbb23e3a269
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-18 01:18:18 -0700

    Instructor note

--- converge.py
+++ converge.py
@@ -3,11 +3,11 @@
 
 def compute_sum(kValue, tolValue):
     final_sum=0
-    next_sum=((1/k)**2)
-    while next_sum > tol:
-        final_sum = (final_sum + next_sum)
+    next_sum=((1/k)**2)                     ### k is undefined here
+    while next_sum > tol:                   ### tol is undefined here
+        final_sum = (final_sum + next_sum)  ### you could use += here
         k+=1
-        next_sum = ((1/k)**2)
+        next_sum = ((1/k)**2)               ### you could define next_sum(k) as a function of k
     return(final_sum)
 
 #for k = .5 on lower and lower tolerances, compute_sum approaches 5)
